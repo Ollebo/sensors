250ebcb4bc595248d19fd1d7864a1a8f7e6e481e
Mattias Hemmingsson <matte.hemingsson@gmail.com>
2023-01-12 07:03:30 +0000

    Updated with working temp sensor

--- temp/run.py
+++ temp/run.py
@@ -1,23 +1,53 @@
 import Adafruit_DHT
 import time
+import json
+from flask import Flask, request, render_template, url_for, redirect
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET'])
+def root():
+    return "Home for temp"
 
 
 
+@app.route("/metrics", methods = ['GET'])
+def metrics():
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            tempdata = "{0:0.1f}".format(temperature)
+            humidata = "{0:0.1f}".format(humidity)
+            premethues_data = "sensor_data_temp "+str(tempdata)+" \nsensor_data_humidity "+str(humidata)
+            f = open("/midevices/devices-temp.json", "w")
+            data = '"temp":"{0:0.1f}", "humidity":"{1:0.1f}"'.format(temperature, humidity)
+            f.write("{"+data+"}")
+            f.close()
+            return premethues_data
+        else:
+            return "Error getting data "
 
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+@app.route("/json", methods = ['GET'])
+def json():
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        f = open("/midevices/devices-temp.json", "w")
-        data = '"temp":"{0:0.1f}", "humidity":"{1:0.1f}"'.format(temperature, humidity)
-        f.write("{"+data+"}")
-        f.close()
-        time.sleep(4)
-    else:
-        print("Failed to retrieve data from humidity sensor")
+            f = open("/midevices/devices-temp.json", "w")
+            data = '"temp":"{0:0.1f}", "humidity":"{1:0.1f}"'.format(temperature, humidity)
+            f.write("{"+data+"}")
+            f.close()
+            json_data = "{"+data+"}"
+            return json_data
+        else:
+            return "Error getting data "
+
+@app.route("/live", methods = ['GET'])
+def live():
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            return "Working"
+        else:
+            return "Error getting data "
--- temp/run.sh
+++ temp/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Starting up temp"
-python3 -u /code/run.py
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+FLASK_APP=run.py flask run --host=0.0.0.0 --port=8080
